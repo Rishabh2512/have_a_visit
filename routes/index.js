a0a8855c7f6a58e234e406c1b9097902e1e87f4a
Rishabh2512 <rishabhtripathi2529@gmail.com>
2020-08-09 04:38:49 +0000

    Add a button in login page for sign up

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.post("/register",function(req,res){
 		else
 		{
 		passport.authenticate("local")(req,res,function(){
-			req.flash("success","Welcome to YelpCamp  "+user.username)
+			req.flash("success","Welcome to Have_a_Visit  "+user.username)
 			res.redirect("/campgrounds");
 		})
 		}
